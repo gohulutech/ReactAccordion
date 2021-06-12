b2ad12b6b6b590f9d187e78ccdb92b98331ea421
Hugo Arevalo <hugoarevalo@Hugos-MacBook-Pro.local>
2021-06-11 20:17:41 -0500

    Removed commented code

--- src/components/Search.js
+++ src/components/Search.js
@@ -33,55 +33,6 @@ const Search = () => {
     if (debouncedTerm) search();
   }, [debouncedTerm]);
 
-  //   useEffect(() => {
-  //     const search = async () => {
-  //       const { data } = await axios.get("https://en.wikipedia.org/w/api.php", {
-  //         params: {
-  //           action: "query",
-  //           list: "search",
-  //           origin: "*",
-  //           format: "json",
-  //           srsearch: term,
-  //         },
-  //       });
-
-  //       setResults(data.query.search);
-  //     };
-
-  //     if (term && !results.length) {
-  //       search();
-  //     } else {
-  //       const timeoutId = setTimeout(() => {
-  //         if (term) {
-  //           search();
-  //         }
-  //       }, 500);
-
-  //       return () => {
-  //         clearTimeout(timeoutId);
-  //       };
-  //     }
-  //   }, [term, results.length]);
-
-  //   useEffect(() => {
-  //     const search = async () => {
-  //       const { data } = await axios.get("https://en.wikipedia.org/w/api.php", {
-  //         params: {
-  //           action: "query",
-  //           list: "search",
-  //           origin: "*",
-  //           format: "json",
-  //           srsearch: debouncedTerm,
-  //         },
-  //       });
-
-  //       setResults(data.query.search);
-  //     };
-  //     if (debouncedTerm) {
-  //       search();
-  //     }
-  //   }, [debouncedTerm]);
-
   const renderedResults = results.map((result) => {
     return (
       <div key={result.pageid} className="item">
